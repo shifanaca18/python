22ed3c4051ee1708587deae5fec0f148245dcf8a
shifana <shifanaa@gmail.com>
2023-06-15 16:37:15 +0530

    largest numbers in 3 if and method

--- 7.py
+++ 7.py
@@ -1,10 +1,9 @@
 a=int(input("enter 3 number"))
 b=int(input())
 c=int(input())
-if(a>b):
-    if(a>c):
+if a>b and a>c:
         print(a,"is largest number")
-elif(b>c):
+elif b>c:
     print(b,"is largest number")
 else:
     print(c,"is largest number")
