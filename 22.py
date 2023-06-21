963003f850a1e6909108f3202fe5b74ff53f74bd
bishermuhammed <bisherp2@gmail.com>
2023-06-21 16:31:22 +0530

    15 14 13...

---
+++ 22.py
@@ -0,0 +1,6 @@
+s=15
+for a in range(1,6):
+    for b in range(6,a,-1):
+        print(s,end=" ")
+        s=s-1
+    print()
---
+++ 23.py
@@ -0,0 +1,4 @@
+for a in range(1,6):
+    for b in range(a,-1):
+        print(a,end=" ")
+    print()
