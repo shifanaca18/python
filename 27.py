af79ef13076fe705552d39ad16cbe6fb32bd65b7
shifana <shifana@gmail.com>
2023-06-26 16:47:56 +0530

    name add,delete,edit

--- 27.py
+++ 27.py
@@ -1,19 +1,23 @@
 name=['A','B','C','D']
 n=int(input("enter a number"))
-# if n==1:
-#     name.append(input("enter a name"))
-#     print(name)
-
-# elif n==2:
-#     a=(input("enter name"))
-#     del name
-#     print(name)
-if n==3:
-    name=['A','B','C','D']
-    b=(input("enter name"))
+if n==1:
+    name.append(input("enter a name"))
+    print(name)
+elif n==3:
+    a=input("enter name")
+    if a in name:
+        del name[name.index(a)]
+        print("Delleted Success")
+        print(name)
+    else:
+        print("Data not available")
+        print(name)
+if n==2:
+    b=input("enter name")
     if b in name:
-        input("enter edited name")
-        name[b]=b
-    print(b)
-
+        c=input("enter edited name")
+        name[name.index(b)]=c
+        print(name)
+    else:
+        print("name not valid")
 
---
+++ 28.py
@@ -0,0 +1,19 @@
+name=['A','B','C','D']
+n=int(input("enter a number"))
+if n==1:
+    name.append(input("enter a name"))
+    print(name)
+
+elif n==2:
+    a=(input("enter name"))
+    del name
+    print(name)
+if n==3:
+    name=['A','B','C','D']
+    b=input("enter name")
+    if b in name:
+        input("enter edited name")
+        name[b]=b
+    print(b)
+
+
