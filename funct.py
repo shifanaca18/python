36bf3243bbea9282dd2b518f68ad6f3a2bad157b
shifana <shifana@gmail.com>
2023-06-28 16:06:57 +0530

    + - / * in functions

---
+++ funct.py
@@ -0,0 +1,23 @@
+def data():
+    global a
+    a=int(input("enter 2 number"))
+    global b
+    b=int(input())
+def sum():
+    c=a+b
+    print("sum",c)
+def sub():
+    c=a-b
+    print("sub",c)
+def multy():
+    c=a*b
+    print("multy",c)
+def div():
+    c=a/b
+    print("division :",c)
+data()
+sum()
+multy()
+div()
+sub()
+
