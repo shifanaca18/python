c62b69648f0635da9902a50e5dd3b7005e6f1c2b
bishermuhammed <bisherp2@gmail.com>
2023-07-19 16:59:36 +0530

    constructures

--- 38.py
+++ 38.py
@@ -1,5 +1,27 @@
 class First:
-    def first(self):
-        print("Hello World")
-x=First()
-x.first()
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def sum(self):
+        self.c=self.a+self.b
+        print("sum",self.c)
+
+    def mul(self):
+        self.d=self.a*self.b
+        print("multy",self.d)
+    def div(self):
+        self.e=self.a/self.b
+        print("division",self.e)
+    def sub(self):
+        self.f=self.a-self.b
+        print("Sub",self.f)
+
+
+a=int(input("enter 2 number"))
+b=int(input())
+x=First(a,b)
+
+x.sum()
+x.mul()
+x.div()
+x.sub()
